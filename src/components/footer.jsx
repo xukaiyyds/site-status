4079082c5ced664cfe24b129b4480bd36c15fb2a
xukai <3353212714@qq.com>
2024-08-19 13:04:53 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -33,6 +33,7 @@ const Footer = () => {
           <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
           &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
         </p>
+        <!--
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
@@ -44,6 +45,7 @@ const Footer = () => {
             </React.Fragment>
           ) : null}
         </p>
+        -->
       </div>
     </footer>
   );
